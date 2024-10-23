3a69a86819b721d2408a7b260b434fd2f25251da
Bernarda <bernarda.schwalba@gmail.com>
2024-10-23 17:26:50 +0200

    Drugi zadatak

--- Main.java
+++ Main.java
@@ -1,45 +1,57 @@
-/*1. Zadatak – Zamjena najmanjeg i najvećeg broja u List-i
-Zadatak:
-Napiši program koji od korisnika traži unos niza brojeva i sprema ih u List-u.
-Pronađi najmanji i najveći broj, zamijeni ih međusobno i ispiši novu List-u.
-*/
+
+
+/*/*
+2. Zadatak. Od korisnika trazite unos 10 brojeva te u listi pronadite koji broj se najvise puta ponavlja.
+Za rezultat ispisite broj koji se najvise puta ponavljao u listi te koliko se puta pojavio u listi.
+Treba uzeti u obriz da su svi brojevi razliciti.*/
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         List<Integer> listaBrojeva = new ArrayList<>();
 
-        for (int i=0; i<5; i++) {
-            System.out.print("Unesite broj: ");
-            listaBrojeva.add(scanner.nextInt());
-        }
+        int najcesciBroj = 0;
+        int najvisePonavljanja = 0;
 
-        int minIndex = 0;
-        int maxIndex = 0;
-        Integer minBroj = listaBrojeva.get(0);
-        Integer maxBroj = listaBrojeva.get(0);
+        System.out.println("Unesite 10 brojeva:");
+
+        for (int i=0; i<10; i++) {
+            System.out.print("Unesite " + (i + 1) + " broj: ");
+            int broj = scanner.nextInt();
 
 
-        for (int i=0; i<listaBrojeva.size(); i++) {
-            if (listaBrojeva.get(i) < minBroj) {
-                minBroj = listaBrojeva.get(i);
-                minIndex = i;
-            }
-
-            if (listaBrojeva.get(i) > maxBroj) {
-                maxBroj = listaBrojeva.get(i);
-                maxIndex = i;
+            if (!listaBrojeva.contains(broj)) {
+                listaBrojeva.add(broj);
+            } else {
+                System.out.println("Uneseni broj već postoji. Unesite drugi broj.");
+                i--;
             }
         }
 
-        listaBrojeva.set(minIndex, maxBroj);
-        listaBrojeva.set(maxIndex, minBroj);
+        for (int i = 0; i < listaBrojeva.size(); i++) {
+            int trenutniBroj = listaBrojeva.get(i);
+            int brojac = 1;
 
-        System.out.println("Nova izmijenjena lista: " + listaBrojeva);
+            for (int j=0; j<listaBrojeva.size(); j++) {
+                if (i!=j && trenutniBroj == listaBrojeva.get(j)) {
+                    brojac++;
+
+                }
+            }
+        }
+
+        if (najvisePonavljanja > 1) {
+            System.out.println("Broj s najviše ponavljanja: " + najcesciBroj);
+            System.out.println("Broj se ponovio: " + najvisePonavljanja);
+
+        } else {
+            System.out.println("Svi uneseni brojevi su različiti.");
+        }
     }
 }
